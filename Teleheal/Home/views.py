c9ee9ee35d75abc0093d123c478ab02f16360bc6
Bryium <bryiumonyancha@gmail.com>
2024-11-04 10:57:02 +0300

    create a login_view

--- Teleheal/Home/views.py
+++ Teleheal/Home/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-
 # Create your views here.
 
 def home(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render({}, request))
+
+def login_view(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render({}, request))  
