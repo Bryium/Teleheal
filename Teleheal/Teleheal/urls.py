05134784515218dfddc756655573e132fcfdf823
Bryium <bryiumonyancha@gmail.com>
2024-11-04 10:40:26 +0300

    add index.html file

--- Teleheal/Teleheal/urls.py
+++ Teleheal/Teleheal/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from Teleheal.Home import views
+from Home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
