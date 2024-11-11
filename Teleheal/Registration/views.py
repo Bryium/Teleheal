4b7e74be5a64d99a3ae6c1dd19fc619fdd57ba5a
Bryium <bryiumonyancha@gmail.com>
2024-11-11 22:24:45 +0300

    update views.py registration file

--- Teleheal/Registration/views.py
+++ Teleheal/Registration/views.py
@@ -1,33 +1,28 @@
-# registration/views.py
+import json
 
-from django.shortcuts import render, redirect
-from django.views import View
-from .models import User
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import User  
 
-class RegistrationView(View):
-    def get(self, request):
-        # Render the registration form template
-        return render(request, 'register.html')
+@csrf_exempt
+def register(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        name = data.get("name")
+        email = data.get("email")
+        password = data.get("password")
+        age = data.get("age")
+        gender = data.get("gender")
+        country = data.get("country")
 
-    def post(self, request):
-        # Retrieve data from the form
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        age = request.POST.get('age')
-        gender = request.POST.get('gender')
-        country = request.POST.get('country')
+        # Simple validation, if required, could be added here
+        if not (name and email and password and age and gender and country):
+            return JsonResponse({"success": False, "message": "All fields are required."})
 
-        # Save the new user to the database
-        user = User(
-            name=name,
-            email=email,
-            password=password,  # Ideally, hash the password for security
-            age=age,
-            gender=gender,
-            country=country
-        )
+        # Save data to the database
+        user = User(name=name, email=email, password=password, age=age, gender=gender, country=country)
         user.save()
 
-        # Redirect to the login page after successful registration
-        return redirect('login')  # This should be the name of your login URL
+        return JsonResponse({"success": True})
+    return JsonResponse({"success": False, "message": "Invalid request method"})
