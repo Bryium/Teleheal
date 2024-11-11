8bf9f396de587960b28b8a010aa0a181c1ba2015
Bryium <bryiumonyancha@gmail.com>
2024-11-11 23:25:44 +0300

    change the views.py file

--- Teleheal/Registration/urls.py
+++ Teleheal/Registration/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-from .views import register
 
 urlpatterns = [
-    path('', register, name='register'),
-    ]
+    path('', views.register, name='register'),
+    # path('Registration/success/', views.success, name='success'),
+]
--- Teleheal/Registration/views.py
+++ Teleheal/Registration/views.py
@@ -1,28 +1,27 @@
-import json
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from .models import User
 
-@csrf_exempt
 def register(request):
     if request.method == "POST":
-        data = json.loads(request.body)
-        name = data.get("name")
-        email = data.get("email")
-        password = data.get("password")
-        age = data.get("age")
-        gender = data.get("gender")
-        country = data.get("country")
+        # Retrieve data from the form
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        age = request.POST.get("age")
+        gender = request.POST.get("gender")
+        country = request.POST.get("country")
 
-        # Simple validation, if required, could be added here
+        # Simple validation, if required
         if not (name and email and password and age and gender and country):
             return JsonResponse({"success": False, "message": "All fields are required."})
+
 
         # Save data to the database
         user = User(name=name, email=email, password=password, age=age, gender=gender, country=country)
         user.save()
 
-        return JsonResponse({"success": True})
-    return JsonResponse({"success": False, "message": "Invalid request method"})
+        # Redirect to a success page or render a response
+        return redirect('/Registration/success/')
+
+    return render(request, "register.html")
