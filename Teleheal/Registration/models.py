1b24fcf39139276601670bcfcb05ab9ca56b2823
Bryium <bryiumonyancha@gmail.com>
2024-11-11 22:16:29 +0300

    update the javascript in register page

--- Teleheal/Registration/models.py
+++ Teleheal/Registration/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)  # Consider hashing passwords in practice
+    password = models.CharField(max_length=100)
     age = models.IntegerField()
     gender = models.CharField(max_length=10)
     country = models.CharField(max_length=100)
