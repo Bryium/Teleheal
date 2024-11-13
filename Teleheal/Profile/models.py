0a489e18251bd53bcf5bda668873394c508f9bb1
Bryium <bryiumonyancha@gmail.com>
2024-11-13 21:18:33 +0300

    add models in the profile models

--- Teleheal/Profile/models.py
+++ Teleheal/Profile/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PatientProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    email = models.EmailField()
+    country_of_origin = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
