ce0accad53cec5e6816f6b7418e80a9bd0c8033f
Bryium <bryiumonyancha@gmail.com>
2024-11-13 15:48:13 +0300

    add an onclick function to the navigation menus

--- Teleheal/Home/static/script.js
+++ Teleheal/Home/static/script.js
@@ -8,4 +8,10 @@ menubar.onclick = () => {
 
 }
 
+function toggleMenu() {
+    const  menu = document.querySelector('.menu-links');
+    const  icon = document.querySelector('.hamburger-icon');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
+}
 
