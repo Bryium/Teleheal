443da26e3116c8cedd19746347253a5996b1d317
Bryium <bryiumonyancha@gmail.com>
2024-11-13 21:53:11 +0300

    add a function to fetch user data

--- Teleheal/Profile/views.py
+++ Teleheal/Profile/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 def profile(request):
     # Your logic for rendering the profile page
+    user_profile = request.user
     return render(request, 'profile.html')
